f248734d37b9bd6ad45de4987e41b6ac6b102e4e
rooooooooob <raftias@gmail.com>
2020-05-04 01:33:35 -0500

    DeserializationError + deserialize for wrapper type
    Return our own DeserializationError so we can handle it later on for
    type/group choices, and also have better info to return instead of those
    temporary TODO panics.
    
    Tag-related errors as well as deserialization for wrapper types.

--- static/prelude.rs
+++ static/prelude.rs
@@ -1,6 +1,31 @@
-use cbor_event::{self, de::{Deserialize, Deserializer}, se::{Serialize, Serializer}};
+use cbor_event::{self, de::Deserializer, se::{Serialize, Serializer}};
 use std::io::{BufRead, Write};
 use wasm_bindgen::prelude::*;
+
+pub enum Key {
+    Str(String),
+    Uint(u64),
+}
+
+// we might want to add more info like which field, etc
+pub enum DeserializeError {
+    BreakInDefiniteLen,
+    CBOR(cbor_event::Error),
+    ExpectedNull,
+    MandatoryFieldMissing(Key),
+    TagMismatch{
+        found: u64,
+        expected: u64,
+    },
+    UnknownKey(Key),
+    UnexpectedKeyType(cbor_event::Type),
+}
+
+impl From<cbor_event::Error> for DeserializeError {
+    fn from(err: cbor_event::Error) -> DeserializeError {
+        DeserializeError::CBOR(err)
+    }
+}
 
 // we should probably just generate this directly at the top of serialization.rs
 pub trait SerializeEmbeddedGroup {
@@ -10,11 +35,24 @@ pub trait SerializeEmbeddedGroup {
     ) -> cbor_event::Result<&'a mut Serializer<W>>;
 }
 
+// same as cbor_event::de::Deserialize but with our DeserializeError
+pub trait Deserialize {
+    fn deserialize<R: BufRead>(
+        raw: &mut Deserializer<R>,
+    ) -> Result<Self, DeserializeError> where Self: Sized;
+}
+
+impl<T: cbor_event::de::Deserialize> Deserialize for T {
+    fn deserialize<R: BufRead>(raw: &mut Deserializer<R>) -> Result<T, DeserializeError> {
+        T::deserialize(raw).map_err(|e| DeserializeError::from(e))
+    }
+}
+
 pub trait DeserializeEmbeddedGroup {
     fn deserialize_as_embedded_group<R: BufRead>(
         raw: &mut Deserializer<R>,
         len: cbor_event::Len,
-    ) -> cbor_event::Result<Self> where Self: Sized;
+    ) -> Result<Self, DeserializeError> where Self: Sized;
 }
 
 // CBOR has int = int / nint
@@ -42,3 +80,4 @@ impl cbor_event::se::Serialize for Int {
         }
     }
 }
+// TODO: deserialize
