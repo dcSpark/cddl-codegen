c84a48ed8099e1c513340170dad2fa5de32fee1b
rooooooooob <rooooooooob@users.noreply.github.com>
2023-07-19 12:45:08 -0400

    Document _CDDL_CODEGEN_RAW_BYTES_TYPE_ + trait export (#192)
    * Document _CDDL_CODEGEN_RAW_BYTES_TYPE_ + trait export
    
    The `RawBytesEncoding` trait will now be exported in `serialization.rs`
    when it's used in the CDDL input.
    
    Documented `_CDDL_CODEGEN_RAW_BYTES_TYPE_` usage in the README too.
    
    * README.md update for typo + clarification
    
    * fix tests
    
    * comment in docs folder instead

--- static/error.rs
+++ static/error.rs
@@ -70,6 +70,8 @@ impl DeserializeError {
     }
 }
 
+impl std::error::Error for DeserializeError {}
+
 impl std::fmt::Display for DeserializeError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self.location {
